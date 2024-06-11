7c9c870ab72c6f85eb44890859ef80a2875f5a0d
DevSentory <dev.sentory1989@gmail.com>
2024-06-11 10:54:15 +0900

    init

---
+++ cspm-server/src/test/java/kyobo/cspm/CspmApplicationTests.java
@@ -0,0 +1,12 @@
+package kyobo.cspm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CspmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
