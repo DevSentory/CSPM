7d56e21ac2659e7653da0e981e4606e7af1a7106
DevSentory <dev.sentory1989@gmail.com>
2024-06-11 12:31:01 +0900

    change fileName

--- cspm-server/src/main/java/kyobo/cspm/Application.java
+++ cspm-server/src/main/java/kyobo/cspm/Application.java
@@ -3,21 +3,17 @@ package kyobo.cspm;
 import jakarta.annotation.PostConstruct;
 import kyobo.cspm.utils.AES256;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-public class CspmApplication {
+public class Application {
 
     private final AES256 aes256Util;
 
     public static void main(String[] args) {
-        SpringApplication.run(CspmApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
     /**
